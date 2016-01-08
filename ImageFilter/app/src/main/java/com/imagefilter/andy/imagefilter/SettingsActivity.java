97697db4ff7601029282d005a9b6ea7d5d41ef9e
Andy <aayao@ualberta.ca>
2016-01-07 18:20:03 -0700

    settings UI prototyped

--- ImageFilter/app/src/main/java/com/imagefilter/andy/imagefilter/SettingsActivity.java
+++ ImageFilter/app/src/main/java/com/imagefilter/andy/imagefilter/SettingsActivity.java
@@ -2,12 +2,50 @@ package com.imagefilter.andy.imagefilter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private SeekBar seekBar;
+    private TextView curConvu, newConvu;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        /*
+        Seekbar code modified from http://examples.javacodegeeks.com/android/core/widget/seekbar/android-seekbar-example/
+         */
+        seekBar = (SeekBar) findViewById(R.id.seekConvu);
+        curConvu = (TextView) findViewById(R.id.curConvu);
+        newConvu = (TextView) findViewById(R.id.newConvu);
+
+        //curConvu.setText(); // grab value from shared preferences
+        newConvu.setText("Covered: " + seekBar.getProgress() + "/" + seekBar.getMax());
+
+        seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            int progress = 0;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+                Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                newConvu.setText("Covered: " + progress + "/" + seekBar.getMax());
+                Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
